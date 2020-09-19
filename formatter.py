d089dabe00af8693c0ae0282d92899f388a5ec94
Denomi <71094070+KohimaNayagato@users.noreply.github.com>
2020-09-19 12:01:03 +0100

    Upload formatter.py

---
+++ formatter.py
@@ -0,0 +1,16 @@
+EgapNumber = 1
+
+
+
+with open('input.txt', 'r') as istr:
+    with open('output.txt', 'w') as ostr:
+        for i, line in enumerate(istr):
+            # Get rid of the trailing newline (if any).
+            line = line.rstrip('\n')
+            
+            if i + 1 < 9999999999999999:
+                #line += ' overworld'
+                line = '.waypoint add egap' + str(EgapNumber) + ' ' + line + ' overworld'
+                EgapNumber += 1 
+                
+            print(line, file=ostr)
